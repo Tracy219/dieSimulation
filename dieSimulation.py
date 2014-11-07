c1a56982c322a4816cc1f62904407329f2170f74
Tracy Han <hyn2000219@gmail.com>
2014-11-07 09:27:11 -0500

    Ask side number for each die

--- dieSimulation.py
+++ dieSimulation.py
@@ -10,8 +10,9 @@ from specialInput import int_input
 '''
 
 diceNumber = int_input("How many dice would you like to roll? ")
-sides = int_input("How many sides on your die? ")
+
 
 for i in range(1, diceNumber + 1):
+	sides = int_input("How many sides on your die #{}? ".format(i))
 	result = randint(1, sides)
 	print("Die {} shows: {}".format (i, result))
