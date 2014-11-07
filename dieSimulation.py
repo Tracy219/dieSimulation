781ce01bed7983c3c76eb76577395be8b4264145
Tracy Han <hyn2000219@gmail.com>
2014-11-07 08:59:12 -0500

    Ask sides and import specialInput

--- dieSimulation.py
+++ dieSimulation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from random import randint
+from specialInput import int_input
 
-result = randint(1, 6)
+sides = int_input("How many sides on your die? ")
+result = randint(1, sides)
+
 print("The die shows: {}".format(result))
